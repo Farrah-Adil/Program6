93a6f6c1da3b2c7053632ac41ed4a386d7e88f6c
Farah Adil <fadil@cedrus.com>
2023-07-21 16:01:57 +0500

    Commit Feat: Program6 is done

---
+++ Program6/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+
+public class Example
+{
+    public static void Main()
+    {
+        int[] arr = { 8, 3, 5, -1, 2 };
+        Console.WriteLine("Minimum number is " + arr.Min());
+        Console.WriteLine("Maximum number is " + arr.Max());
+    }
+}
