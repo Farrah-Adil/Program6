f153e74a87e74cc4d97c5e176952897950800991
Farah Adil <fadil@cedrus.com>
2023-07-21 16:05:10 +0500

    Commit Feat: done successfully

--- Program6/Program.cs
+++ Program6/Program.cs
@@ -1,13 +1,22 @@
-﻿
-using System;
-
-
-public class Example
+﻿namespace program;
+ class MaxMinNUm
 {
     public static void Main()
     {
         int[] arr = { 8, 3, 5, -1, 2 };
         Console.WriteLine("Minimum number is " + arr.Min());
         Console.WriteLine("Maximum number is " + arr.Max());
+
+
+        int[] array = { 4, 2, 3, -1, -2 };
+        Console.WriteLine("Minimum number is " + arr.Min());
+        Console.WriteLine("Maximum number is " + arr.Max());
+
+
+
+
+
+
+
     }
 }
